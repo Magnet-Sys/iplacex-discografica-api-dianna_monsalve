59f9ce607f70adc2107c48b91491538f4eb1c7be
Magnet-Sys <dianna.monsalve@gmail.com>
2025-09-21 00:32:05 -0300

    First commit

---
+++ src/main/java/org/iplacex/proyectos/discografia/discos/DiscoController.java
@@ -0,0 +1,71 @@
+package org.iplacex.proyectos.discografia.discos;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.iplacex.proyectos.discografia.artistas.IArtistaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class DiscoController {
+  @Autowired
+  private IDiscoRepository discoRepo;
+
+  @Autowired
+  private IArtistaRepository artistaRepo;
+
+  @PostMapping(
+    value = "/disco", 
+    consumes = MediaType.APPLICATION_JSON_VALUE, 
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public ResponseEntity<Object> handlePostDiscoRequest(@RequestBody Disco disco) {
+    if (!artistaRepo.existsById(disco.idArtista)) {
+        return new ResponseEntity<>("El artista no existe", HttpStatus.NOT_FOUND);
+    }
+    Disco temp = discoRepo.insert(disco);
+    return new ResponseEntity<>(temp, HttpStatus.CREATED);
+  }
+
+  @GetMapping(
+    value = "/discos", 
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public ResponseEntity<List<Disco>> handleGetDiscosRequest() {
+      List<Disco> discos = discoRepo.findAll();
+      return new ResponseEntity<>(discos, null, HttpStatus.OK);
+  }
+
+  @GetMapping(
+    value = "/disco/{id}", 
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public ResponseEntity<Object> handleGetDiscoRequest(@PathVariable("id") String id) {
+      Optional<Disco> temp = discoRepo.findById(id);
+      if (!temp.isPresent()) {
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+      }
+      return new ResponseEntity<>(temp.get(), null, HttpStatus.OK);
+  }
+
+  @GetMapping(
+    value = "/artista/{id}/discos", 
+    produces = MediaType.APPLICATION_JSON_VALUE
+  )
+  public ResponseEntity<List<Disco>> handleGetDiscosByArtistaRequest(@PathVariable("id") String id) {
+      List<Disco> discos = discoRepo.findDiscosByIdArtista(id);
+      return new ResponseEntity<>(discos, HttpStatus.OK);
+  }
+}
